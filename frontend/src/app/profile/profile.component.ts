3f32e2d0dc82e0919060262118295186642c233f
Utkarsh Tiwari <uttiwariun0910@gmail.com>
2021-09-28 11:54:04 +0530

    Minor Changes

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -14,9 +14,12 @@ export class ProfileComponent implements OnInit {
 
   ngOnInit(): void {
     const storage = localStorage.getItem('google_auth');
+    const storage2 = localStorage.getItem('facebook_auth');
 
     if (storage) {
       this.userDetails = JSON.parse(storage);
+    } else if (storage2) {
+      this.userDetails = JSON.parse(storage2);
     } else {
       this.signOut();
     }
@@ -24,6 +27,7 @@ export class ProfileComponent implements OnInit {
 
   signOut(): void {
     localStorage.removeItem('google_auth');
+    localStorage.removeItem('facebook_auth');
     this.router.navigateByUrl('/authenticate').then();
   }
 
